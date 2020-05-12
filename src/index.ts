9b7a31effc0035ca90ba765362df45a9bc485bef
Felix Sargent <felix.sargent@gmail.com>
2020-05-12 01:10:16 -0700

    percent time is just better.

--- src/index.ts
+++ src/index.ts
@@ -4,34 +4,38 @@ require('dotenv').config();
 const tz = process.env.TZ || 'America/Los_Angeles';
 
 const decimalize = (timeFrame: string): String => {
+  // Prints the % completed of the timeframe requested. 'day','month','year' etc
   const timeArray = Math.trunc(
     ((moment().tz(tz) - moment().tz(tz).startOf(timeFrame)) /
       (moment().tz(tz).endOf(timeFrame) - moment().tz(tz).startOf(timeFrame))) *
-    100000
-  ).toString().padStart(5, '0').split('');
-  timeArray.splice(1, 0, ':');
-  timeArray.splice(4, 0, ':');
+    1000000
+  ).toString().padStart(6, '0').split('');
+  timeArray.splice(2, 0, ':');
+  timeArray.splice(5, 0, ':');
   return timeArray.join('');
 };
 
 const duodecimalTime = (): String => {
+  // Prints duodecimal time
   const now = moment().tz(tz);
-  return [`${(now.hour() >= 12) ? '1' : ' '}`,
+  return [`${(now.hour() >= 12) ? '1' : '0'}`,
     `${now.format('h').replace('10', 'X').replace('11', 'E').replace('12', '0')}`,
     `:${now.format('mm:ss')}`].join('');
 };
 
 const message = () => {
-  const line1 = [` ${decimalize('day')}%`,
+  const line1 = [`${decimalize('day')}%`,
     `    `,
     `${moment().format('ddd')}`,
   ].join('');
+
   const line2 = [
     `${duodecimalTime()}`,
 
     `   `,
     `${moment().format('M')}m${moment().format('DD')}d`,
   ].join('');
+
   if (line1.toString().length != 16|| line2.toString().length != 16) {
     console.error(`line1: ${line1.toString().length}`);
     console.error(`line2: ${line2.toString().length}`);
